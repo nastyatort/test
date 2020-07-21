ffae7a65bb90318479a030a79ca1383d0c33e636
amoshenskaya <amoshenskaya@cmlatitude.com>
2020-07-21 15:14:33 +0300

    minor fixes

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@ const menuBtn = document.getElementById('menu-btn');
 const menuList = document.getElementById('menu-list');
 
 const burger = () => {
-  console.log('burger');
-
   if(menuList.classList.contains('show')){
     menuList.classList.remove('show');
     menuBtn.textContent = 'show'
@@ -11,7 +9,20 @@ const burger = () => {
     menuList.classList.add('show');
     menuBtn.textContent = 'hide'
   }
+}
 
+const outsideBurger = (e) => {
+
+  const menuListWrapper = menuList.contains(e.target);
+
+  if(menuList.classList.contains('show')){
+    if(e.target !== menuList && e.target != menuBtn && !menuListWrapper) {
+      menuList.classList.remove('show');
+      menuBtn.textContent = 'show'
+    }
+  }
 }
 
 menuBtn.addEventListener('click', burger);
+
+document.addEventListener('click', outsideBurger)
